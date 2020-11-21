88277080beb060eaef97d9ac4fbc3bc12612c438
marvkal <marvka+github@protonmail.com>
2020-11-21 09:45:37 +0000

    Add Book constructor

---
+++ script.js
@@ -0,0 +1,8 @@
+const myLibrary = [];
+
+function Book(title, author, pageCount, haveRead) {
+  this.title = title;
+  this.author = author;
+  this.pageCount = pageCount;
+  this.haveRead = haveRead;
+}
