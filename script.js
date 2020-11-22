da4eb20989e881c33467adfdb13dee6af2d13c6e
marvkal <marvka+github@protonmail.com>
2020-11-22 11:15:23 +0000

    Add prototype function for creating HTML Elements
    This function will be used to create DOM elements that will later be appended to individual book containers.

--- script.js
+++ script.js
@@ -7,6 +7,26 @@ function Book(title, author, pageCount, haveRead) {
   this.haveRead = haveRead;
 }
 
+Book.prototype.createHTMLElement = function (key) {
+  const element = document.createElement('p');
+  switch (key) {
+    case 'title':
+      element.innerHTML = `<p><strong>${this.title}</strong></p>`;
+      break;
+    case 'author':
+      element.innerHTML = `<p>${this.author}</p>`;
+      break;
+    case 'pageCount':
+      element.innerHTML = `<p>${this.pageCount} Pages</p>`;
+      break;
+    case 'haveRead':
+      const read = this.haveRead ? 'Read' : 'Unread';
+      element.innerHTML = `<button id="read-button">${read}</button>`;
+      break;
+  }
+  return element;
+};
+
 function addBookToLibrary(title, author, pageCount, haveRead) {
   myLibrary.push(new Book(title, author, pageCount, haveRead));
 }
