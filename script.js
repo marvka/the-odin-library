a904b624a3ef3df878f6ea356c954c4825a5da8f
marvka <marvka@tutanota.com>
2021-04-27 19:59:58 +0100

    Rewrite using classes

--- script.js
+++ script.js
@@ -1,157 +1,145 @@
-const myLibrary = [];
+class Library {
+  constructor() {
+    this.books = [];
+  }
 
-function Book(title, author, pageCount, haveRead) {
-  this.title = title;
-  this.author = author;
-  this.pageCount = pageCount;
-  this.haveRead = haveRead;
+  addBook(book) {
+    this.books.push(book);
+  }
+
+  removeBook(index) {
+    this.books.splice(index, 1);
+  }
 }
 
-Book.prototype.createHTMLElement = function (key) {
-  const element = document.createElement('p');
-  switch (key) {
-    case 'title':
-      element.innerHTML = `<p><strong>${this.title}</strong></p>`;
-      break;
-    case 'author':
-      element.innerHTML = `<p>${this.author}</p>`;
-      break;
-    case 'pageCount':
-      element.innerHTML = `<p>${this.pageCount} Pages</p>`;
-      break;
+class Book {
+  constructor(title, author, pageCount, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pageCount = pageCount;
+    this.isRead = isRead;
   }
-  return element;
-};
 
-Book.prototype.toggleReadStatus = function () {
-  if (this.haveRead) {
-    this.haveRead = false;
-  } else {
-    this.haveRead = true;
+  toggleIsRead() {
+    this.isRead === 'Read' ? (this.isRead = 'Unread') : (this.isRead = 'Read');
   }
-};
-
-function addBookToLibrary(title, author, pageCount, haveRead) {
-  const newBook = new Book(title, author, pageCount, haveRead);
-  myLibrary.push(newBook);
-  displayNewBook(newBook);
 }
 
-function displayNewBook(book) {
-  const libraryContainer = document.getElementById('library-container');
-  const bookContainer = document.createElement('div');
-  bookContainer.classList.add('book-container');
-  bookContainer.dataset.index = myLibrary.indexOf(book);
+class DisplayController {
+  constructor() {
+    this.library = document.getElementById('library');
+    this.overlay = document.getElementById('overlay');
+    this.newBook = document.getElementById('new-book-button');
+    this.cancel = document.getElementById('cancel');
+    this.addBook = document.getElementById('add-book');
 
-  for (const key in book) {
-    if (book.hasOwnProperty(key)) {
-      if (key === 'haveRead') {
-        const read = book[key] ? 'Read' : 'Unread';
+    this.formTitle = document.getElementById('title');
+    this.formAuthor = document.getElementById('author');
+    this.formPageCount = document.getElementById('page-count');
+    this.formUnReadRadio = document.getElementById('unread-radio');
+    this.formError = document.getElementById('input-error');
 
-        const readButton = document.createElement('button');
-        readButton.classList.add('read-button');
-        readButton.textContent = read;
-        readButton.addEventListener('click', toggleReadStatusButton);
+    this.newBook.addEventListener('click', this.showOverlay.bind(this));
+    this.cancel.addEventListener('click', this.hideOverlay.bind(this));
+    this.addBook.addEventListener('click', this.readInput.bind(this));
+  }
 
-        bookContainer.appendChild(readButton);
-      } else {
-        bookContainer.appendChild(book.createHTMLElement(key));
+  updateDisplay() {
+    console.log(this.library.hasChildNodes());
+    this.resetDisplay();
+
+    // Loop over books and create required elements for each property
+    for (const index in myLibrary.books) {
+      const book = myLibrary.books[index];
+      const container = document.createElement('div');
+      container.classList.add('book-container');
+      for (const property in book) {
+        const paragraph = document.createElement('p');
+        paragraph.textContent = book[property];
+        container.appendChild(paragraph);
       }
+
+      const toggleReadButton = document.createElement('button');
+      toggleReadButton.classList.add('read-button');
+      toggleReadButton.textContent = 'Toggle Read';
+      const removeButton = document.createElement('button');
+      removeButton.classList.add('remove-book');
+      removeButton.textContent = 'Remove';
+      removeButton.addEventListener('click', () => {
+        myLibrary.removeBook(index);
+        this.updateDisplay();
+      });
+      toggleReadButton.addEventListener('click', () => {
+        book.toggleIsRead();
+        this.updateDisplay();
+      });
+
+      container.appendChild(toggleReadButton);
+      container.appendChild(removeButton);
+
+      this.library.appendChild(container);
+      console.log(this.library.hasChildNodes());
     }
   }
 
-  const removeButton = document.createElement('button');
-  removeButton.classList.add('remove-book');
-  removeButton.addEventListener('click', removeBook);
-  removeButton.textContent = 'Remove';
-  bookContainer.appendChild(removeButton);
-
-  libraryContainer.appendChild(bookContainer);
-}
-
-function showOverlay() {
-  document.getElementById('overlay').style.display = 'block';
-}
-
-function hideOverlay() {
-  document.getElementById('overlay').style.display = 'none';
-}
-
-function toggleReadStatusButton(event) {
-  const bookContainer = event.target.parentNode;
-  myLibrary[bookContainer.dataset.index].toggleReadStatus();
-  event.target.textContent = myLibrary[bookContainer.dataset.index].haveRead
-    ? 'Read'
-    : 'Unread';
-}
-
-function removeBook(event) {
-  const bookContainer = event.target.parentNode;
-  const index = bookContainer.dataset.index;
-  delete myLibrary[index];
-  bookContainer.remove();
-}
-
-function initializeEventListeners() {
-  // "New Book" button
-  document
-    .getElementById('new-book-button')
-    .addEventListener('click', showOverlay);
-
-  // "Cancel" button
-  document.getElementById('cancel').addEventListener('click', hideOverlay);
-
-  // Esc key
-  window.addEventListener('keydown', (e) => {
-    if (/(Escape)/.test(e.key)) {
-      hideOverlay();
+  resetDisplay() {
+    if (this.library.hasChildNodes()) {
+      this.library.textContent = '';
     }
-  });
-
-  // "Add Book" button
-  document.getElementById('add-book').addEventListener('click', readInput);
-}
-
-function validateInput(title, author, pageCount) {
-  const errorElement = document.getElementById('input-error');
-  if (!title) {
-    errorElement.textContent = 'Please enter a title!';
-    errorElement.style.display = 'block';
-    return false;
-  } else if (!author) {
-    errorElement.textContent = 'Please enter an author!';
-    errorElement.style.display = 'block';
-    return false;
-  } else if (!pageCount) {
-    errorElement.textContent = 'Please enter the number of pages!';
-    errorElement.style.display = 'block';
-    return false;
   }
-  return true;
-}
 
-function resetForm() {
-  document.getElementById('title').value = '';
-  document.getElementById('author').value = '';
-  document.getElementById('page-count').value = '';
-  document.getElementById('unread').checked = true;
-  document.getElementById('input-error').textContent = '';
-  document.getElementById('input-error').style.display = 'none';
-}
+  showOverlay() {
+    this.overlay.style.display = 'block';
+  }
 
-function readInput() {
-  const title = document.getElementById('title').value;
-  const author = document.getElementById('author').value;
-  const pageCount = parseInt(document.getElementById('page-count').value);
-  const haveRead =
-    document.querySelector('input[name="read-unread"]:checked').value ===
-    'true';
-  console.log(haveRead);
+  hideOverlay() {
+    this.overlay.style.display = 'none';
+  }
 
-  if (validateInput(title, author, pageCount, haveRead)) {
-    addBookToLibrary(title, author, pageCount, haveRead);
-    resetForm();
+  validateInput(title, author, pageCount) {
+    const errorElement = document.getElementById('input-error');
+    if (!title) {
+      errorElement.textContent = 'Please enter a title!';
+      errorElement.style.display = 'block';
+      return false;
+    } else if (!author) {
+      errorElement.textContent = 'Please enter an author!';
+      errorElement.style.display = 'block';
+      return false;
+    } else if (!pageCount) {
+      errorElement.textContent = 'Please enter the number of pages!';
+      errorElement.style.display = 'block';
+      return false;
+    }
+    return true;
+  }
+
+  resetForm() {
+    this.formTitle.value = '';
+    this.formAuthor.value = '';
+    this.formPageCount.value = '';
+    this.formUnReadRadio.checked = true;
+    this.formError.textContent = '';
+    this.formError.style.display = 'none';
+  }
+
+  readInput() {
+    const title = this.formTitle.value;
+    const author = this.formAuthor.value;
+    const pageCount = parseInt(this.formPageCount.value);
+    const isRead = document.querySelector('input[name="read-unread"]:checked')
+      .value;
+
+    if (this.validateInput(title, author, pageCount)) {
+      myLibrary.addBook(new Book(title, author, pageCount, isRead));
+      this.resetForm();
+      this.updateDisplay();
+    }
   }
 }
 
-initializeEventListeners();
+const myLibrary = new Library();
+const displayController = new DisplayController();
+const testBook = new Book('Linux Adventure', 'Marvin', 452, 'Unread');
+myLibrary.addBook(testBook);
+displayController.updateDisplay();
